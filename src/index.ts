7caa66b31869e5f2fb535b2a32b8f8c6a564c862
Bruno Kilian <bkilian@twilio.com>
2024-06-10 19:16:00 +0200

    start sentry

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import * as FlexPlugin from '@twilio/flex-plugin';
+import { startSentry } from './sentry';
 
+import * as FlexPlugin from '@twilio/flex-plugin';
 import FlexSentryPlugin from './FlexSentryPlugin';
+
+startSentry();
 
 FlexPlugin.loadPlugin(FlexSentryPlugin);
---
+++ src/sentry.ts
@@ -0,0 +1,27 @@
+import { createBrowserHistory } from 'history';
+
+import * as Sentry from '@sentry/react';
+
+export const startSentry = () => {
+  const history = createBrowserHistory();
+
+  // Array of Route Config Objects
+  // Make sure the order of the routes is correct. The longest url under the same parent should be placed first and in decreasing order.
+  // Doc: https://docs.sentry.io/platforms/javascript/guides/react/features/react-router/
+  const routes = [{ path: '/agent-desktop/:taskReservation' }, { path: '/agent-desktop' }, { path: '/' }];
+
+  Sentry.init({
+    dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0', // ADD YOUR SENTRY DSN HERE
+    integrations: [
+      Sentry.reactRouterV5BrowserTracingIntegration({
+        history,
+        routes,
+        // matchPath,
+      }),
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+};
